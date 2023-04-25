03cfeaa73caa52dfaaef3f915aa2a9d75bcd79a8
golevartemohta <golevartem1995@gmail.com>
2023-04-25 11:12:22 +0200

    lesson32-start activity shop item

---
+++ app/src/main/java/com/example/shopliststudyproject/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package com.example.shopliststudyproject.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.shopliststudyproject.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
